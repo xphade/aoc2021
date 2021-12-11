cda220932d989d64bf894e65f938df9908cc8cac
xphade <18196286+xphade@users.noreply.github.com>
2021-12-11 16:44:18 +0100

    Fix `run-all` script (add leading zero)

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+# run-all.sh - Run all (currently implemented) days
 
 cd "$(dirname "$0")/.."
 echo
 
-for i in {1..25}; do
+for i in {01..25}; do
     # Continue if file does not (yet) exist
-    file=(./*${i}_*.py)
+    file=(./${i}_*.py)
     if [[ ! -f ${file[0]} ]]; then
         continue
     fi
 
     echo "Day $i:"
-    eval "./*${i}_*.py data/*${i}_*.txt"
+    eval "./${i}_*.py data/${i}_*.txt"
     echo
 done
